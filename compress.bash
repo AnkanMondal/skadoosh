c89c0f0037acd58c9053ef84c498df4dcbd9766f
AnkanMondal <31277215+AnkanMondal@users.noreply.github.com>
2017-09-07 16:45:21 +0530

    Update compress.bash
    AOSP Oreo

--- compress.bash
+++ compress.bash
@@ -8,7 +8,7 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=AOSP
+name=AOSP_Oreo
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
@@ -16,7 +16,7 @@ manifest=https://android.googlesource.com/platform/manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=android-7.1.2_r27
+branch=android-8.0.0_r4
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
